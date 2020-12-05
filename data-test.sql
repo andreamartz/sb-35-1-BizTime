901d8355481e8ba85c5c7eaa0ef9530f486c7224
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-12-04 22:33:31 -0600

    Add industry information to route that gets a company; also add passing tests for this route

--- data-test.sql
+++ data-test.sql
@@ -4,8 +4,10 @@ CREATE DATABASE biztime_test;
 
 \c biztime_test
 
-DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS invoices CASCADE;
+DROP TABLE IF EXISTS companies CASCADE;
+DROP TABLE IF EXISTS industries CASCADE;
+DROP TABLE IF EXISTS companies_industries CASCADE;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -23,12 +25,38 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
--- INSERT INTO companies
---   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
---          ('ibm', 'IBM', 'Big blue.');
+CREATE TABLE industries (
+    code TEXT PRIMARY KEY,
+    industry TEXT UNIQUE NOT NULL
+);
 
--- INSERT INTO invoices (comp_Code, amt, paid, paid_date)
---   VALUES ('apple', 100, false, null),
---          ('apple', 200, false, null),
---          ('apple', 300, true, '2018-01-01'),
---          ('ibm', 400, false, null);
+CREATE TABLE companies_industries (
+    comp_code TEXT NOT NULL REFERENCES companies ON DELETE CASCADE,
+    ind_code TEXT NOT NULL REFERENCES industries ON DELETE CASCADE,
+    PRIMARY KEY(comp_code, ind_code)
+);
+
+INSERT INTO companies
+  VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
+         ('ibm', 'IBM', 'Big blue.');
+
+INSERT INTO invoices (comp_Code, amt, paid, paid_date)
+  VALUES ('apple', 100, false, null),
+         ('apple', 200, false, null),
+         ('apple', 300, true, '2018-01-01'),
+         ('ibm', 400, false, null);
+
+INSERT INTO industries (code, industry)
+  VALUES ('acct', 'Accounting'),
+         ('hr', 'Human Resources'),
+         ('qa', 'Quality Assurance'),
+         ('eng', 'Software Engineering'),
+         ('hw', 'Hardware'),
+         ('mech', 'Mechanical'),
+         ('tr', 'Trades');
+
+INSERT INTO companies_industries (comp_code, ind_code)
+  VALUES ('apple', 'qa'),
+         ('apple', 'eng'),
+         ('ibm', 'hw'),
+         ('ibm', 'eng');
